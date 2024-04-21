b2b9c9d5ebadcba3c3f7d2260c24dc9f797b48bc
shooozz <vodolb@mail.ru>
2024-04-21 19:42:27 +0300

    fix something in pagination, but its not all

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function App() {
     const [searchValue, setSearchValue] = React.useState('')
 
     return (
-        <div className="wrapper">
+        <div className='wrapper'>
             <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-            <div className="content">
-                <div className="container">
+            <div className='content'>
+                <div className='container'>
                     <Routes>
-                        <Route path="/" element={<Home searchValue={searchValue} setSearchValue={setSearchValue} />} />
-                        <Route path="/cart" element={<Cart />} />
-                        <Route path="*" element={<NotFound />} />
+                        <Route path='/' element={<Home searchValue={searchValue} setSearchValue={setSearchValue} />} />
+                        <Route path='/cart' element={<Cart />} />
+                        <Route path='*' element={<NotFound />} />
                     </Routes>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+
 import Categories from '../components/Categories'
 import Sort from '../components/Sort'
 import PizzaBlock from '../components/PizzaBlock'
@@ -8,33 +9,35 @@ import Pagination from '../components/Pagination'
 
 const Home = ({ searchValue, setSearchValue }) => {
     const [pizzasData, setPizzasData] = React.useState([])
-    const [isLoaded, setIsLoaded] = React.useState(true)
+    const [isLoaded, setIsLoaded] = React.useState(false)
     const [indexCategories, setIndexCategories] = React.useState(0)
     const [currentPage, setCurrentPage] = React.useState(0)
     const [selectedSort, setSelectedSort] = React.useState({ name: 'популярности', sortProperty: 'rating' })
+    const [offsetLoaded, setOffsetLoaded] = React.useState(false)
 
     const url = 'https://api.airtable.com/v0/appeSnv5U5vIOIJCq/reactPizza0'
     const API_KEY = 'pat3n4HF2M6n8Yc7t.a2bf9f82332b5826eb455b6800df4dd44ce26d0777881c5352925e057aec86c0'
     const pageSize = 4
     const [offset, setOffset] = React.useState([''])
+    const [tempOffset, setTempOffset] = React.useState('')
 
     React.useEffect(() => {
-        setIsLoaded(true)
+        setIsLoaded(false)
 
         const responseHandler = data => {
             setPizzasData(
                 data.map(pizzaData => {
-                    pizzaData.fields['types'] = JSON.parse(pizzaData.fields.types)
-                    pizzaData.fields['sizes'] = JSON.parse(pizzaData.fields.sizes)
+                    pizzaData.fields.types = JSON.parse(pizzaData.fields.types)
+                    pizzaData.fields.sizes = JSON.parse(pizzaData.fields.sizes)
                     return pizzaData.fields
-                }),
+                })
             )
         }
 
         const offsetHandler = response => {
-            if (offset.indexOf(response) !== -1) {
-                console.log('Finded')
-            } else if (offset.indexOf(response) === -1) {
+            if (typeof response === 'undefined') {
+                setOffsetLoaded(true)
+            } else {
                 setOffset(prevOffset => [...prevOffset, response])
             }
         }
@@ -43,26 +46,47 @@ const Home = ({ searchValue, setSearchValue }) => {
         const sortBy = selectedSort.sortProperty.replace('-', '')
         const categoryFetch =
             indexCategories > 0
-                ? `filterByFormula=%7Bcategory%7D%3D${indexCategories}&sort%5B0%5D%5Bfield%5D=${sortBy}&sort%5B0%5D%5Bdirection%5D=${sortOrder}`
+                ? `filterByFormula=%7Bcategory%7D%3D${indexCategories}&
+                sort%5B0%5D%5Bfield%5D=${sortBy}&sort%5B0%5D%5Bdirection%5D=${sortOrder}`
                 : `&sort%5B0%5D%5Bfield%5D=${sortBy}&sort%5B0%5D%5Bdirection%5D=${sortOrder}&pageSize=${pageSize}&offset=${
                       currentPage ? offset[currentPage] : ''
                   }`
 
+        // while (!offsetLoaded) {
+        //     axios
+        //         .get(
+        //             `${url}?
+        // &sort%5B0%5D%5Bfield%5D=${sortBy}&sort%5B0%5D%5Bdirection%5D=${sortOrder}&pageSize=${pageSize}&offset=${tempOffset}`,
+        //             {
+        //                 headers: {
+        //                     Authorization: `Bearer ${API_KEY}`
+        //                 }
+        //             }
+        //         )
+        //         .then(response => {
+        //             console.log('CHECK')
+        //             if (!offsetLoaded) {
+        //                 offsetHandler(response.data.offset)
+        //                 setTempOffset(response.data.offset)
+        //             }
+        //         })
+        // }
+
         axios({
             method: 'get',
             url: `${
-                searchValue
-                    ? `${url}?sort%5B0%5D%5Bfield%5D=${sortBy}&sort%5B0%5D%5Bdirection%5D=${sortOrder}`
-                    : `${url}?${categoryFetch}`
+                searchValue ? `${url}?sort%5B0%5D%5Bfield%5D=${sortBy}&sort%5B0%5D%5Bdirection%5D=${sortOrder}` : `${url}?${categoryFetch}`
             }`,
             headers: {
-                Authorization: `Bearer ${API_KEY}`,
-            },
+                Authorization: `Bearer ${API_KEY}`
+            }
         }).then(response => {
-            console.log(offset)
-            offsetHandler(response['data']['offset'])
-            responseHandler(response['data']['records'])
-            setIsLoaded(false)
+            console.log(response)
+            responseHandler(response.data.records)
+            setIsLoaded(true)
+            if (!offsetLoaded) {
+                offsetHandler(response.data.offset)
+            }
         })
         window.scrollTo(0, 0)
     }, [indexCategories, selectedSort, searchValue, currentPage])
@@ -79,7 +103,7 @@ const Home = ({ searchValue, setSearchValue }) => {
         .map(pizzaObj => <PizzaBlock {...pizzaObj} key={pizzaObj['id']} />)
     return (
         <>
-            <div className="content__top">
+            <div className='content__top'>
                 <Categories
                     value={indexCategories}
                     onClickCategory={index => setIndexCategories(index)}
@@ -87,13 +111,10 @@ const Home = ({ searchValue, setSearchValue }) => {
                 />
                 <Sort value={selectedSort} onClickSort={index => setSelectedSort(index)} />
             </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">{isLoaded ? skeletons : pizzas}</div>
+            <h2 className='content__title'>Все пиццы</h2>
+            <div className='content__items'>{isLoaded ? pizzas : skeletons}</div>
 
-            <Pagination
-                onChangePage={number => setCurrentPage(number)}
-                setIndexCategories={() => setIndexCategories(0)}
-            />
+            <Pagination onChangePage={number => setCurrentPage(number)} setIndexCategories={() => setIndexCategories(0)} />
         </>
     )
 }
